3e4bb155f5db21409da23efc9bc047901b2b70f5
zazons <tiago.ogomes.2004@gmail.com>
2022-04-04 12:42:14 +0100

    Lista

---
+++ app/src/main/java/com/quackplayer/FilesList.java
@@ -0,0 +1,37 @@
+package com.quackplayer;
+
+import android.net.Uri;
+
+public class FilesList {
+    private boolean isPlaying;
+    private final String title, artist, duration;
+    private final Uri fileUri;
+
+    public FilesList(boolean isPlaying, String title, String artist, String duration, Uri fileUri) {
+        this.isPlaying = isPlaying;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.fileUri = fileUri;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public Uri getFileUri() {
+        return fileUri;
+    }
+}
