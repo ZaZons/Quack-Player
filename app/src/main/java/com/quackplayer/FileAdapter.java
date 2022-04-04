04e3cf2a7d968763e6633b50a8127bce85f47c4b
zazons <tiago.ogomes.2004@gmail.com>
2022-04-04 12:42:39 +0100

    Adaptador e layout

---
+++ app/src/main/java/com/quackplayer/FileAdapter.java
@@ -0,0 +1,70 @@
+package com.quackplayer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder> {
+
+    private final List<FilesList> filesList;
+    private final SelectFileListener selectFileListener;
+
+    public FileAdapter(List<FilesList> filesList, Context context) {
+        this.filesList = filesList;
+        this.selectFileListener = ((SelectFileListener)context);
+    }
+
+    @SuppressLint("InflateParams")
+    @NonNull
+    @Override
+    public FileViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new FileViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.files_adapter_layout, null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FileViewHolder holder, int position) {
+        FilesList selectedFile = filesList.get(position);
+
+        holder.title.setText(selectedFile.getTitle());
+        holder.artist.setText(selectedFile.getArtist());
+        holder.duration.setText(selectedFile.getDuration());
+
+        if(selectedFile.isPlaying())
+            holder.rootLayout.setBackgroundResource(R.drawable.bg_purple);
+        else
+            holder.rootLayout.setBackgroundResource(R.drawable.background_blue);
+
+        holder.rootLayout.setOnClickListener(v -> selectFileListener.onSelected(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return filesList.size();
+    }
+
+    static class FileViewHolder extends RecyclerView.ViewHolder {
+
+        private final RelativeLayout rootLayout;
+        private final TextView title;
+        private final TextView artist;
+        private final TextView duration;
+
+        public FileViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            rootLayout = itemView.findViewById(R.id.rootLayout);
+            title = itemView.findViewById(R.id.title);
+            artist = itemView.findViewById(R.id.artist);
+            duration = itemView.findViewById(R.id.duration);
+        }
+    }
+}
